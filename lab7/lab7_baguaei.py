81fa9a0c896a08f8a762223346920e1ccf54b085
Nazaneen07 <Baguaein@gmail.com>
2026-02-24 19:10:58 +0000

    notdone

--- lab7/lab7_baguaei.py
+++ lab7/lab7_baguaei.py
@@ -8,5 +8,5 @@ with open("phrases.txt", "r") as file1:
     filecontent = file1.read()
     print(filecontent)
 
-# ccheck if the file is closed 
+# check if the file is closed 
 print(f"Is the file closed? {file1.closed}")
