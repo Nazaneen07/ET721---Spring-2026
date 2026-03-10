2f3db0f641363a3628647bddae0d760379bd798a
Nazaneen07 <Baguaein@gmail.com>
2026-03-10 17:40:56 +0000

    done

---
+++ lab11/app.py
@@ -0,0 +1,32 @@
+"""
+Nazaneen Baguaei 
+Lab 11, introduction ro Flask
+March 10, 2026 
+"""
+from flask import Flask, render_template
+
+"""
+create an object from Flask module 
+"""
+app = Flask(__name__)
+
+# set the routing to the main page 
+# 'route' decorator is used to access the root URL
+@app.route('/')
+def index():
+    name = "Nazaneen Baguaei"
+    fruits = ['apple', 'orange', 'grapes']
+    fruits = 'orange'
+    return render_template('index.html', username = name, listfruits = fruits, f = fruits)
+
+# endpoints refer to the name of the view 
+@app.route('/about')
+def about():
+    return '<h1>About us</h1>'
+@app.route('/quotes')
+def quotes():
+    return '<h1>Quotes</h1>'
+
+# set the 'app' to run if you execute the file directly (not hwen it is imported)
+if __name__ == '__main__':
+    app.run(debug=True)
